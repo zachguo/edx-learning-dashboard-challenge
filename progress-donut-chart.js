d1e8e59ef4c9e8b850d105ce2537fd8541602161
Zach Guo <zachguo@gmail.com>
2015-01-21 11:20:03 -0800

    default report

--- progress-donut-chart.js
+++ progress-donut-chart.js
@@ -2,64 +2,71 @@
   // fake data
   var datasource = {
     all: [{
+      parent: "overall",
       name: "week1",
       val: 0.95
     }, {
+      parent: "overall",
       name: "week2",
       val: 1
     }, {
+      parent: "overall",
       name: "week3",
       val: 0.55
     }, {
+      parent: "overall",
       name: "week4",
       val: 0.95
     }, {
+      parent: "overall",
       name: "week5",
       val: 0.45
     }, {
+      parent: "overall",
       name: "week6",
       val: 0
     }, {
+      parent: "overall",
       name: "week7",
       val: 0
     }],
     week1: [{
-      parent: "all",
+      parent: "week1",
       name: "lecture1",
       val: 1
     }, {
-      parent: "all",
+      parent: "week1",
       name: "lecture2",
       val: 0.9
     }],
     week2: [{
-      parent: "all",
+      parent: "week2",
       name: "lecture3",
       val: 1
     }, {
-      parent: "all",
+      parent: "week2",
       name: "lecture4",
       val: 1
     }],
     week3: [{
-      parent: "all",
+      parent: "week3",
       name: "lecture5",
       val: 0.4
     }, {
-      parent: "all",
+      parent: "week3",
       name: "lecture6",
       val: 0.7
     }],
     lecture1: [{
-      parent: "week1",
+      parent: "lecture1",
       name: "v1",
       val: 1
     }, {
-      parent: "week1",
+      parent: "lecture1",
       name: "v2",
       val: 1
     }, {
-      parent: "week1",
+      parent: "lecture1",
       name: "v3",
       val: 1
     }]
@@ -84,7 +91,19 @@
       return 1; // equally segmented
     });
 
+  var defaultreport = group.append("text")
+    .attr("text-anchor", "middle");
+
+  update(datasource.all);
+
   function update(data) {
+    // generate default report
+    if (data[0]) {
+      defaultreport
+      // .attr("opacity", 1)
+        .text(generateReport(data[0].parent, 1));
+    }
+
     var arcs = group.selectAll(".arc")
       .data(pie(data)); // get angles from pie layout
 
@@ -113,11 +132,10 @@
       .attr("d", arcFG);
 
     arcs.append("text")
-      .attr("transform", "translate(0,0)")
       .attr("text-anchor", "middle")
       .attr("opacity", 0)
       .text(function(d) {
-        return d.data.name + ": " + d.data.val * 100 + "%";
+        return generateReport(d.data.name, d.data.val);
       });
 
     arcs.exit().remove();
@@ -126,10 +144,12 @@
       // show corresponding text & highlight hovered arc when hovering an arc
       .on("mouseover", function() {
         var currentSelector = d3.select(this.parentNode).attr("opacity", 0.8);
+        defaultreport.attr("opacity", 0);
         currentSelector.select("text").attr("opacity", 1);
       })
       .on("mouseout", function() {
         var currentSelector = d3.select(this.parentNode).attr("opacity", 1);
+        defaultreport.attr("opacity", 1);
         currentSelector.select("text").attr("opacity", 0);
       })
       // zoom in when clicking an arc
@@ -143,12 +163,13 @@
         }
       });
 
-    // TODO default text report when mouseout
     // TODO zoom out (a back-to-parent button in text report?)
     // TODO transition
 
   }
 
-  update(datasource.all);
+  function generateReport(label, val) {
+    return label + ": " + val * 100 + "%";
+  }
 
 })();
